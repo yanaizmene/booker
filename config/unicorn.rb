3952d0b71277b50e0cd0914958bc82120ec0db9f
yanaizmene <zhyrny@gmail.com>
2014-05-18 00:38:20 +0300

    uni conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,17 +1,7 @@
-development:
-  working_directory "/home/yanaizmene/www/rails/booker"
-  pid "/home/yanaizmene/www/rails/booker/pids/unicorn.pid"
-  stderr_path "/home/yanaizmene/www/rails/booker/log/unicorn.log"
-  stdout_path "/home/yanaizmene/www/rails/booker/log/unicorn.log"
-  listen "/tmp/unicorn.booker.sock"
-  worker_processes 2
-  timeout 30
-
-test:
-  working_directory "/apps/booker/current"
-  pid "/apps/booker/current/pids/unicorn.pid"
-  stderr_path "/apps/booker/current/log/unicorn.log"
-  stdout_path "/apps/booker/current/log/unicorn.log"
-  listen "/tmp/unicorn.booker.sock"
-  worker_processes 2
-  timeout 30
+working_directory "/home/yanaizmene/www/rails/booker"
+pid "/home/yanaizmene/www/rails/booker/pids/unicorn.pid"
+stderr_path "/home/yanaizmene/www/rails/booker/log/unicorn.log"
+stdout_path "/home/yanaizmene/www/rails/booker/log/unicorn.log"
+listen "/tmp/unicorn.booker.sock"
+worker_processes 2
+timeout 30
