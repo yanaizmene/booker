8138c775c164312074d08a91fff1fe41dca815d4
yanaizmene <zhyrny@gmail.com>
2014-05-18 00:29:19 +0300

    test/dev uni conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,3 +6,12 @@ development:
   listen "/tmp/unicorn.booker.sock"
   worker_processes 2
   timeout 30
+
+test:
+  working_directory "/apps/booker/current"
+  pid "/apps/booker/current/pids/unicorn.pid"
+  stderr_path "/apps/booker/current/log/unicorn.log"
+  stdout_path "/apps/booker/current/log/unicorn.log"
+  listen "/tmp/unicorn.booker.sock"
+  worker_processes 2
+  timeout 30
